81921d2f8514f5887c72b6715ec550aceaf26adf
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-06-21 16:05:20 +0800

    add CORS config

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,13 @@ module Xyston
     config.active_record.raise_in_transactional_callbacks = true
 
     config.active_job.queue_adapter = :delayed_job
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
   end
 end
