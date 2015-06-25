be0efd5cb95a1ac12a1d03a755184c8b3793be52
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-06-25 20:45:32 +0800

    add product model bare bones along with some validations

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+  let(:product) { build :product }
+  subject { product }
+
+  it { should respond_to(:title) }
+  it { should respond_to(:price) }
+  it { should respond_to(:published) }
+  it { should respond_to(:user_id) }
+
+  it { should_not be_published }
+
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :user_id }
+  it { should validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+end
