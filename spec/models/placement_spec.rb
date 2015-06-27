6719abba9dea9104799a1d9e72e5ef33976b49f0
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-06-27 23:38:21 +0800

    decrements the product quantity by the placement quantity

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 RSpec.describe Placement, type: :model do
   let(:placement) { build :placement }
 
-  it { should respond_to :order_id }
-  it { should respond_to :product_id }
-  it { should respond_to :quantity }
+  describe "validation" do
 
-  it { should belong_to :order }
-  it { should belong_to :product }
+    it { should respond_to :order_id }
+    it { should respond_to :product_id }
+    it { should respond_to :quantity }
+
+    it { should belong_to :order }
+    it { should belong_to :product }
+  end
+
+  describe "#decrement_product_quantity!" do
+    it "decreases the product quantity by the placement quantity" do
+      expect { placement.decrement_product_quantity! }.to change{ placement.product.quantity }.by(-placement.quantity)
+    end
+  end
 end
